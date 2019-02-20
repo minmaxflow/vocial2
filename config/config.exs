240b5c43434ef5ced98e15e4c3dcd0b630026af5
喻林 <yulin_yl@126.com>
2019-02-20 14:07:48 +0800

    mx phx.new

---
+++ config/config.exs
@@ -0,0 +1,27 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :vocial,
+  ecto_repos: [Vocial.Repo]
+
+# Configures the endpoint
+config :vocial, VocialWeb.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "42GiG/5DqhoQ5/ueoi3+wrWVS+aVF9G7E5POAGZ/y0L7EIJ1Mr8jD1p5sSPrpGJi",
+  render_errors: [view: VocialWeb.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Vocial.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
