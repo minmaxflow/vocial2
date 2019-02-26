246b923b070ac252b4550b35e001ba0729b5a505
喻林 <yulin_yl@126.com>
2019-02-26 15:45:01 +0800

    validate

--- lib/vocial/accounts/user.ex
+++ lib/vocial/accounts/user.ex
@@ -24,7 +24,18 @@ defmodule Vocial.Accounts.User do
     |> cast(attrs, [:username, :email, :active, :password, :password_confirmation])
     |> validate_confirmation(:password, message: "does not match password!")
     |> encrypt_password()
+    |> validate_not_fake(:email)
     |> validate_required([:username, :email, :active, :encrypted_password])
+    |> validate_format(:email, ~r/@/)
+    |> validate_length(:username, min: 3, max: 100)
+    |> unique_constraint(:username)
+  end
+
+  def validate_not_fake(changeset, key) do 
+    case get_change(changeset, key) do 
+      "test@fake.com" -> add_error(changeset, key, "cannot be a fake email")
+      _ -> changeset
+    end
   end
 
   def encrypt_password(changeset) do 
--- test/vocial/accounts/account_test.exs
+++ test/vocial/accounts/account_test.exs
@@ -56,5 +56,26 @@ defmodule Vocial.AccountsTest do
       assert is_nil(Accounts.get_user_by_username("fail"))
     end
 
+    test "create_user/1 fails to create the user when the username alread exists" do 
+      _user1 = user_fixture()
+      {:error, user2} = user_fixture()
+      assert !user2.valid?
+    end
+
+    test "create_user/1 fails to create the user when email is not an email format" do 
+      {:error, user} = user_fixture(%{email: "testemail"})
+      assert !user.valid?
+    end
+
+    test "create_user/1 fails to create the user when email is fake" do 
+      {:error, user} = user_fixture(%{email: "test@fake.com"})
+      assert !user.valid?
+    end
+
+    test "create_user/1 fails to create the user when the username is too short" do 
+      {:error, user} = user_fixture(%{username: "a"})
+      assert !user.valid?
+    end
+
   end
 end
